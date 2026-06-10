4e322250d148dd86337e74a6ab58ec6b9af15a21
Suraj <sura@skinrange.comj>
2026-06-10 16:20:10 +0530

    updated 1 project

--- brandway/settings.py
+++ brandway/settings.py
@@ -128,7 +128,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
